ae5e7032b9344070f21b5094b2c720c8896e8cdf
Mintaek2 <789rkd@naver.com>
2023-01-09 16:48:40 +0900

    짝수는 싫어요

---
+++ programmers/level-0/짝수는 싫어요.js
@@ -0,0 +1,11 @@
+const solution = (n) =>{
+    let answer = [];
+    for(let i = 1; i <= n; i++) {
+        if(i % 2 === 1) {
+            answer.push(i);
+        }
+    }
+    return answer; 
+}
+
+console.log(solution(10))
