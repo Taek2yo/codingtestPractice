7f6354ed88c5e53d7c5d55e9a0f901386c2344a3
Mintaek2 <789rkd@naver.com>
2023-01-12 16:25:36 +0900

    나이, reverse, 아메리카노, 할인

---
+++ programmers/level-0/나이 출력.js
@@ -0,0 +1,6 @@
+// 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+// 0 < age <= 120
+const solution = (age) => 2023 - age
+
+// 이거 너무 알고리즘 처럼 안푼듯..
+// 출제자의 의도는 new Date().getFullYear() - age 인듯.. 
