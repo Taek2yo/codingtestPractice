6991e762ee0ad008ce2fcb5c5345ce7f9ddbd96b
Mintaek2 <789rkd@naver.com>
2023-01-16 15:43:23 +0900

    순서

---
+++ programmers/level-0/순서쌍.js
@@ -0,0 +1,15 @@
+// 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+// 자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+
+function solution(n) {
+  let answer = 0;
+  for (let i = 0; i < n + 1; i++) {
+    if (n % i === 0) {
+      answer++;
+    }
+  }
+  return answer;
+}
+
+
+// 약수 구하기 . 나누어 떨어지는 = 나머지가 없다.
