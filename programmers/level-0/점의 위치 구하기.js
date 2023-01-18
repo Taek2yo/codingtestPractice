2e1b873700e2b2c1501969d99e0819a5f41b2f04
Mintaek2 <789rkd@naver.com>
2023-01-18 19:53:38 +0900

    조건문, 배열, 수학, 시뮬레이션

---
+++ programmers/level-0/점의 위치 구하기.js
@@ -0,0 +1,22 @@
+function solution(dot) {
+  let answer = 0;
+  if (dot[0] > 0 && dot[1] > 0) {
+    answer = 1;
+  } else if (dot[0] < 0 && dot[1] > 0) {
+    answer = 2;
+  } else if (dot[0] < 0 && dot[1] < 0) {
+    answer = 3;
+  } else if (dot[0] > 0 && dot[1] < 0) {
+    answer = 4;
+  }
+  return answer;
+}
+
+// 피드백
+// 하드코딩인 것 같아서 푼 뒤에 다른 사람 풀이를 보니 구조 분해를 하였다.
+
+function solution(dot) {
+  const [a, b] = dot;
+  const check = a * b > 0;
+  return a > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+}
