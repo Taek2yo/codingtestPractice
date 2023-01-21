1b8daa16d09aa7115d377e95c8e5283c4576dba4
Mintaek2 <789rkd@naver.com>
2023-01-21 16:56:06 +0900

    문자열, 정렬, 사칙연산, 수학

---
+++ programmers/level-0/숨어있는 숫자의 덧셈(1).js
@@ -0,0 +1,8 @@
+// 문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+function solution (my_string){
+    const arr = [...my_string].map(Number).filter((e)=>!isNaN(e))
+    let answer = arr.reduce((a,c)=>a+c);
+    return answer;
+}
+
+console.log(solution("aAb1B2cC34oOp"))
