239861e55410874c9be50582f0e2af3996cde6fe
Mintaek2 <789rkd@naver.com>
2023-02-24 17:59:37 +0900

    2023-02-24 완료

---
+++ programmers/level-1/[1차]-비밀지도.js
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17681
+
+function solution(n, arr1, arr2) {
+  let num1, num2, s;
+  let answer = [];
+  for (let i = 0; i < n; i++) {
+    num1 = arr1[i];
+    num2 = arr2[i];
+    s = "";
+    for (let j = 0; j < n; j++) {
+      s = (num1 % 2) + (num2 % 2) ? "#" + s : " " + s;
+      num1 = Math.floor(num1 / 2);
+      num2 = Math.floor(num2 / 2);
+    }
+    answer.push(s);
+  }
+  return answer;
+}
+
+console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
