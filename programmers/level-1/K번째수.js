82036545ddce6075cca6fe2ee33ad7fc49f6fa41
Mintaek2 <789rkd@naver.com>
2023-02-27 18:12:57 +0900

    2023-02-27 완료

--- programmers/level-1/K번째수.js
+++ programmers/level-1/K번째수.js
@@ -13,4 +13,14 @@ function solution(array, commands) {
     return answer;
 }
 
+
+// map() 풀이
+function solution2(array, commands) {
+    return commands.map(
+        ([from, to, k]) =>
+            array.slice(from - 1, to).sort((a, b) => a - b)[k - 1],
+    );
+}
+
+
 console.log(solution([1, 5, 2, 6, 3, 7, 4],	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
