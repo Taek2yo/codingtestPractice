54aeabdc194fe040d2e12a9c5022d4c4d3069a03
Mintaek2 <789rkd@naver.com>
2023-03-14 23:12:30 +0900

    2023-03-14 완료

---
+++ programmers/level-1/카드-뭉치.js
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/159994
+
+function solution(cards1, cards2, goal) {
+
+    for(let s of goal) {
+
+        if(cards1[0] == s) {
+            cards1.shift();
+        } else if(cards2[0] == s) {
+            cards2.shift();
+        } else {
+            return "No"
+        }
+    }
+
+    return "Yes";
+}
