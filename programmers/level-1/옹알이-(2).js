f13e9ad9f6bcf20d5c3209f488482560aee69930
Mintaek2 <789rkd@naver.com>
2023-03-15 17:07:54 +0900

    2023-03-15 완료

---
+++ programmers/level-1/옹알이-(2).js
@@ -0,0 +1,25 @@
+// 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고 연속해서 같은 발음을 하는 것을 어려워합니다. 
+// 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+
+function solution(babbling) {
+    const can = ['aya','ye','woo','ma'];
+    let count = 0;
+    
+    for(let i = 0; i < babbling.length; i++){
+        let babble = babbling[i];
+        
+        for(let j = 0; j < can.length; j++){
+            if(babble.includes(can[j].repeat(2))){
+                break;
+            }
+            
+            babble = babble.split(can[j]).join(" ");
+        }
+        
+        if(babble.split(" ").join("").length === 0){
+            count += 1;
+        }
+    }
+    
+    return count;
+}
