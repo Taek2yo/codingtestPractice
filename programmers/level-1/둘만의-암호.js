2d5a10190e8b682395a7e9bee08769e568743307
Mintaek2 <789rkd@naver.com>
2023-03-23 16:58:37 +0900

    2023-03-23 완료

---
+++ programmers/level-1/둘만의-암호.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/155652
+
+function solution(s, skip, index) {
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+                      "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+                      "u", "v", "w", "x", "y", "z"].filter(c => !skip.includes(c));
+    return s.split("").map(c => alphabet[(alphabet.indexOf(c) + index) % alphabet.length]).join("");
+}
+
+console.log(solution("aukks","wbqd",5))
