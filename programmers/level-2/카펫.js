c90c32a1c3e0584834e7923dd38f70b1f88de808
Mintaek2 <789rkd@naver.com>
2023-04-06 19:16:28 +0900

    2023-04-06 완료

---
+++ programmers/level-2/카펫.js
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42842
+
+function solution(brown, yellow) {
+  for (var y = 3; y <= (brown + yellow) / y; y++) {
+    var x = Math.floor((brown + yellow) / y);
+    if ((x - 2) * (y - 2) === yellow) {
+      break;
+    }
+  }
+
+  return [x, y];
+}
+
+console.log(solution(10, 2));
