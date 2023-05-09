4e6c77c51043b8c82ffd9afc2fa9e2ba55ea8f0d
Mintaek2 <789rkd@naver.com>
2023-05-09 19:16:19 +0900

    2023-05-09 완료

---
+++ programmers/기초-트레이닝/공백으로-구분하기2.js
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181868
+
+function solution(my_string) {
+   return my_string.split(" ").filter((e,i)=> e)
+}
+
+console.log(solution(" i    love  you"))
