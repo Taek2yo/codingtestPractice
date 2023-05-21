7a2ff38a6d31bdb183a3b6a6672efbb0f1f12722
Mintaek2 <789rkd@naver.com>
2023-05-21 11:18:48 +0900

    2023-05-21 완료

---
+++ programmers/기초-트레이닝/수-조작하기2.js
@@ -0,0 +1,24 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181925
+
+function solution(numLog) {
+    let answer = '';
+  
+    for (let i = 1; i < numLog.length; i++) {
+      const diff = numLog[i] - numLog[i - 1];
+  
+      if (diff === 1) {
+        answer += 'w';
+      } else if (diff === -1) {
+        answer += 's';
+      } else if (diff === 10) {
+        answer += 'd';
+      } else if (diff === -10) {
+        answer += 'a';
+      }
+    }
+  
+    return answer;
+  }
+  
+
+console.log(solution([0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1]))
