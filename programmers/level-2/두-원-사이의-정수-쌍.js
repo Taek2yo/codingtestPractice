f5c8f77e9f5a3b42dc354b9b4ef760b15b5e91cd
Mintaek2 <789rkd@naver.com>
2023-07-22 13:21:00 +0900

    2023-07-22 완료

---
+++ programmers/level-2/두-원-사이의-정수-쌍.js
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181187
+
+function solution(r1, r2) {
+  var answer = 0;
+  for (var x = 1; x <= r2; x++) {
+    var maxY = Math.floor(Math.sqrt(r2 ** 2 - x ** 2));
+    if (r1 > x) {
+      var minY = Math.ceil(Math.sqrt(r1 ** 2 - x ** 2));
+    } else {
+      var minY = 0;
+    }
+    answer += maxY - minY + 1;
+  }
+  return answer * 4;
+}
+console.log(solution(2, 3));
